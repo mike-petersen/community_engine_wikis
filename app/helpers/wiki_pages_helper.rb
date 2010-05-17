0aa87f12d0af98dbb91d65a5e29f9f5c3ef0f935
Mike Petersen <mike@odania-it.de>
2010-05-17 23:53:26 +0200

    Updated Regular Expression

--- app/helpers/wiki_pages_helper.rb
+++ app/helpers/wiki_pages_helper.rb
@@ -1,7 +1,7 @@
 module WikiPagesHelper
 	def format_wiki_page(wiki, content)
-		content = content.gsub(/\[\[(\w+)\]\]/) {|match| match = match.gsub("[[", "").gsub("]]", ""); link_to(match, wiki_wiki_page_path(wiki, :id => match.parameterize.downcase))}
-		content = content.gsub(/\[\[(\w+)\|(\w+)\]\]/) {|match| tmp = match.gsub("[[", "").gsub("]]", "").split("|"); link_to(tmp[1], wiki_wiki_page_path(wiki, :id => tmp[0].parameterize.downcase))}
+		content = content.gsub(/\[\[[^\]|]*\]\]/) {|match| match = match.gsub("[[", "").gsub("]]", ""); link_to(match, wiki_wiki_page_path(wiki, :id => match.parameterize.downcase))}
+		content = content.gsub(/\[\[[^\]|]*\|[^\]|]*\]\]/) {|match| tmp = match.gsub("[[", "").gsub("]]", "").split("|"); link_to(tmp[1], wiki_wiki_page_path(wiki, :id => tmp[0].parameterize.downcase))}
 
 		return content
 	end
